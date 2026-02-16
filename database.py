30aef46f51e61e3e701d51342918a3ffa5a5a8b8
Gayathri Valipi Reddy <valipireddygayathri@gmail.com>
2026-02-16 17:36:38 +0530

    Changed Database Credentials

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:Gowtham789@localhost:5432/karyanet"
+db_url = "YourUserName://postgres:YourPassword@localhost:5432/karyanet"
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
